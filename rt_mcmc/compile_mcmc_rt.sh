21e56c2012298b4b6df21fb758ac6be896fc500e
Paulo Cesar Ventura <paulo.pc.vs@gmail.com>
2022-11-22 14:30:59 -0500

    Create variable for compiler command

--- rt_mcmc/compile_mcmc_rt.sh
+++ rt_mcmc/compile_mcmc_rt.sh
@@ -4,7 +4,7 @@ PROJECT_DIR=.   # Use ../.. to compile from the source directory
 SRC_DIR="$PROJECT_DIR"/rtrend_tools/rt_mcmc
 SOURCES_REL="Rt.c"
 GSL_FLAGS="-lgsl -lgslcblas"
-
+COMPILER=gcc
 
 # --- Create full paths (still relative to project_dir)
 for NAME in $SOURCES_REL
@@ -13,4 +13,4 @@ do
 done
 
 # --- Compilation command
-gcc -g -Wall $GSL_FLAGS $SOURCES_ABS -o "$PROJECT_DIR"/main_mcmc_rt
+$COMPILER -g -Wall $GSL_FLAGS $SOURCES_ABS -o "$PROJECT_DIR"/main_mcmc_rt

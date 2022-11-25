ee22c6d96c2c14227e97f057ffd4ce3a1fd51f6b
Paulo Cesar Ventura <paulo.pc.vs@gmail.com>
2022-11-25 10:33:30 -0500

    Add function to get next deadline for submission

--- cdc_params.py
+++ cdc_params.py
@@ -3,7 +3,7 @@ This module holds variables from the CDC requirements for the calculation and fo
 of the data.
 """
 import numpy as np
-import datetime
+from datetime import datetime, timedelta
 
 # --- Parameters common to the Flu and COVID forecasts
 CDC_QUANTILES_SEQ = np.round(np.append(np.append([0.01, 0.025], np.arange(0.05, 0.95 + 0.05, 0.050)), [0.975, 0.99]), 6)
@@ -15,8 +15,24 @@ NUM_STATES = 53  # Number of locations: states and jurisdictions, excluding the 
 WEEKDAY_TGT = 5  # 5 = Saturday
 NUM_OUTP_LINES = NUM_WEEKS_FORE * (NUM_STATES + 1) * NUM_QUANTILES  # Expected output file number of lines
 NUM_OUTP_LINES_WPOINTS = NUM_WEEKS_FORE * (NUM_STATES + 1) * (NUM_QUANTILES + 1)
-REF_DEADLINE = datetime.datetime(2022, 11, 14, 23)  # A reference for submission deadlines (a given Monday 11pm)
+REF_DEADLINE = datetime(2022, 11, 14, 23)  # A reference for submission deadlines (a given Monday 11pm)
 
 # --- COVID parameters
 COV_NUM_STATES = 54  # COVID data includes American Samoa
-COV_REF_DEADLINE = datetime.datetime(2022, 11, 14, 15)  # A reference for submission deadlines (a given Monday 3pm)
+COV_REF_DEADLINE = datetime(2022, 11, 14, 15)  # A reference for submission deadlines (a given Monday 3pm)
+
+
+def _get_next_deadline(ref_deadline, now=None):
+    if now is None:
+        now = datetime.today()
+
+    delta = (now - REF_DEADLINE) % timedelta(weeks=1)  # Time past the last deadline
+    return now - delta + timedelta(weeks=1)
+
+
+def get_next_flu_deadline_from(now=None):
+    return _get_next_deadline(REF_DEADLINE, now)
+
+
+def get_next_cov_deadline_from(now=None):
+    return _get_next_deadline(COV_REF_DEADLINE, now)

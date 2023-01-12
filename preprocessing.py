47fbcccb67158c39c3a488c98f036bb552bd751e
Paulo Cesar Ventura <paulo.pc.vs@gmail.com>
2023-01-12 11:53:33 -0500

    Implement noise preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -7,13 +7,14 @@ import numpy as np
 import pandas as pd
 from scipy.signal import butter, filtfilt
 
-from rtrend_tools.forecast_structs import ForecastExecutionData, ForecastOutput, AbstractNoise, CovHospForecastOutput
+from rtrend_tools.forecast_structs import ForecastExecutionData, AbstractNoise, CovHospForecastOutput
 
 
 # ----------------------------------------------------------------------------------------------------------------------
 # AUX METHODS
 # ----------------------------------------------------------------------------------------------------------------------
 def apply_lowpass_filter(signal_array, cutoff=0.4, order=2):
+    """Return a Butterworth lowpass filtered time series, with same dimension as input data."""
     if len(signal_array.shape) > 1:
         warnings.warn(f"Hey, a {len(signal_array.shape):d}D array/df was passed to 'apply_lowpass_filter', but 1D is "
                       f"expected. The filtered data may be silently wrong.")
@@ -23,33 +24,35 @@ def apply_lowpass_filter(signal_array, cutoff=0.4, order=2):
 
 
 # ----------------------------------------------------------------------------------------------------------------------
-# DENOISING METHODS (COVID-HOSP FOCUSED)(remember to update DENOISE CALLABLE dict)
+# DENOISING METHODS (COVID-HOSP FOCUSED) (remember to update DENOISE CALLABLE dict)
 # ----------------------------------------------------------------------------------------------------------------------
 # noinspection PyUnusedLocal
 def denoise_lowpass(exd: ForecastExecutionData, fc: CovHospForecastOutput, filt_order=2, cutoff=0.4, **kwargs):
-    fc.denoised_weekly = pd.Series(apply_lowpass_filter(exd.state_series.values, cutoff, filt_order),
-                                   index=exd.state_series.index)
+    return pd.Series(apply_lowpass_filter(exd.state_series.values, cutoff, filt_order),
+                     index=exd.state_series.index)
 
 
 # noinspection PyUnusedLocal
 def denoise_polyfit(exd: ForecastExecutionData, fc: CovHospForecastOutput, poly_degree=3, **kwargs):
     poly_coef, poly_resid = np.polyfit(fc.t_daily, exd.state_series.values, deg=poly_degree, full=True)[0:2]
     poly_f = np.poly1d(poly_coef)  # Polynomial callable class.
-    fc.denoised_weekly = pd.Series(poly_f(fc.t_daily), index=exd.state_series.index)
+    return pd.Series(poly_f(fc.t_daily), index=exd.state_series.index)
 
 
 # noinspection PyUnusedLocal
-def denoise_rolling_average(exd: ForecastExecutionData, fc: ForecastOutput, rollav_window=4, **kwargs):
-    fc.denoised_weekly = exd.state_series.rolling(rollav_window).mean()  # Rolling average
-    fc.denoised_weekly[:rollav_window-1] = exd.state_series[:rollav_window-1]  # Fill NAN values with original ones
+def denoise_rolling_average(exd: ForecastExecutionData, fc: CovHospForecastOutput, rollav_window=4, **kwargs):
+    denoised = exd.state_series.rolling(rollav_window).mean()  # Rolling average
+    denoised[:rollav_window-1] = exd.state_series[:rollav_window-1]  # Fill NAN values with original ones
     # fc.denoised_weekly[:] *= exd.state_series[-1] / data_weekly[-1] if data_weekly[-1] else 1
-    # #    Rescale to match last day
+    # #                       ^  Rescale to match last day
+
+    return denoised
 
 
 # Dummy method, only redirects to the original time series.
 # noinspection PyUnusedLocal
-def denoise_none(exd: ForecastExecutionData, fc: ForecastOutput, **kwargs):
-    fc.denoised_weekly = exd.state_series
+def denoise_none(exd: ForecastExecutionData, fc: CovHospForecastOutput, **kwargs):
+    return exd.state_series
 
 
 # Dictionary of denoising methods.
